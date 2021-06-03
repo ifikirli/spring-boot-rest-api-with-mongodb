d5c8c1964a5173f7dc799b31b121fc11cbdeab34
ismail.fikirli <ismail.fikirli@ekinokssoftware.com>
2021-06-03 18:10:07 +0300

    initial commit

---
+++ src/main/java/tr/com/ifikirli/sbrestapi/SbrestapiApplication.java
@@ -0,0 +1,13 @@
+package tr.com.ifikirli.sbrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbrestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbrestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tr/com/ifikirli/sbrestapi/SbrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.ifikirli.sbrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbrestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
